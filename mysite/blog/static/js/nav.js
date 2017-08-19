11b9eb717cf8bbfe15b735bfbd4560bc68544bb1
Rohith S Shankar <ruskay.rsk@gmail.com>
2017-08-19 10:38:37 -0400

    'first'

---
+++ mysite/blog/static/js/nav.js
@@ -0,0 +1,8 @@
+// Smooth Scroll on clicking nav items
+$('nav a').click(function () {
+  var $href = $(this).attr('href');
+  $('body').stop().animate({
+    scrollTop: $($href).offset().top
+  }, 1000);
+  return false;
+});
