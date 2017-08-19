daca9bb01806ec4188a63bf8ceffb1676e44655d
Rohith S Shankar <ruskay.rsk@gmail.com>
2017-08-19 12:47:29 -0400

    no message

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'hzetdi)f#0okx$zu5y=7aae$9e6q25#+wmlu%)#=1s9h-m5s&_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ruskay7.pythonanywhere.com']
 
 
 # Application definition
